bd2c9b7962d2b9d28a3d08e67380fe88c3342680
Martin Pilaszek <martinpilaszek@gmail.com>
2021-10-29 22:59:30 -0300

    pagina projects terminada

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="home">
-    Projects
+    <Project
+      v-for="proj in projects"
+      :key="proj.id"
+      :name="proj.title"
+      :img="proj.img"
+      :link="proj.link"
+      :desc="proj.desc"
+    />
   </div>
 </template>
 
 <script>
+import Project from '@/components/Project.vue'
+import { projects } from '@/projects.js'
+
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  components: { Project },
+  data () {
+    return {
+      projects: projects.sort((a, b) => b.id - a.id)
+    }
+  }
 }
 </script>
