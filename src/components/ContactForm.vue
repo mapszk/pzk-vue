7070f106e7a4214e9b5602debfc7f4e79f137632
Martin Pilaszek <martinpilaszek@gmail.com>
2021-10-30 00:01:40 -0300

    Form de contacto terminado

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,158 @@
+<template>
+  <form @submit.prevent="validation">
+    <h2>Contact</h2>
+    <label for="name">Name</label>
+    <input v-model="name" type="text" id="name" name="name">
+    <label for="email">Email</label>
+    <input v-model="email" type="text" id="email" name="email">
+    <label for="message">Message</label>
+    <textarea v-model="message" name="message" id="message" cols="30" rows="10"></textarea>
+    <Button type="submit" mt="1rem" color="secondary" :full="true">Send</Button>
+    <span :class="alertStyles">{{alertMsg}}</span>
+  </form>
+</template>
+
+<script>
+import Button from '@/components/Button.vue'
+
+const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g
+const endpoint = 'https://formspree.io/f/xdoyegpk'
+
+export default {
+  name: 'ContactForm',
+  components: { Button },
+  data () {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      alert: false,
+      alertType: '',
+      alertMsg: '',
+      loading: false
+    }
+  },
+  computed: {
+    alertStyles () {
+      return {
+        visible: this.alert === true,
+        error: this.alertType === 'error',
+        success: this.alertType === 'success'
+      }
+    }
+  },
+  methods: {
+    clearAlert () {
+      setTimeout(() => {
+        this.alert = false
+        this.alertType = ''
+        this.alertMsg = ''
+      }, 5000)
+    },
+    async submit () {
+      this.loading = true
+      await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify({ name: this.name, email: this.email, message: this.message }),
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+        .then(() => {
+          this.alert = true
+          this.alertType = 'success'
+          this.alertMsg = 'Sent! Thank you'
+        })
+        .catch(() => {
+          this.alert = true
+          this.alertType = 'error'
+          this.alertMsg = 'An error has ocurred, please try again'
+        })
+        .finally(() => {
+          this.loading = false
+          this.clearAlert()
+        })
+    },
+    validation () {
+      if (!this.email | !this.name | !this.message) {
+        this.alert = true
+        this.alertType = 'error'
+        this.alertMsg = 'Please fill all the fields'
+        return this.clearAlert()
+      } else if (!emailRegex.test(this.email)) {
+        this.alert = true
+        this.alertType = 'error'
+        this.alertMsg = 'Please put a valid email'
+        return this.clearAlert()
+      } else {
+        this.submit()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2{
+  color: #c8ff30;
+  margin-bottom: .5rem;
+}
+label{
+  display: block;
+  margin-bottom: .3rem;
+  color: #c8ff30;
+}
+input, textarea{
+  display: block;
+  width: 100%;
+  height: 2.5rem;
+  border-radius: 2rem;
+  border: none;
+  padding: 0 1rem;
+  margin-bottom: .6rem;
+}
+input:focus, textarea:focus{
+  border: 2px solid #c8ff30;
+  outline: none;
+}
+textarea{
+  border-radius: 1rem;
+  height: 10rem;
+  resize: none;
+  padding: .8rem;
+}
+form{
+  background-color: #052b36;
+  padding: 1rem;
+  border-radius: 1rem;
+  position: relative;
+}
+
+/* alert */
+span{
+  font-weight: 600;
+  position: absolute;
+  text-align: center;
+  padding: .75rem;
+  left: 0;
+  right: 0;
+  margin-top: 1.5rem;
+  border-radius: 2rem;
+  display: block;
+  transition: transform 1s ease, opacity 1s ease;
+  transform: translateY(20px);
+  opacity: 0;
+}
+.visible{
+  transform: translateY(0);
+  opacity: 1;
+}
+.error {
+  background-color: red;
+  color: white;
+}
+.success {
+  background-color: #c8ff30;
+  color: #052b36;
+}
+</style>
