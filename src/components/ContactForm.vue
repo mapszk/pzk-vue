d2ccee297fdae7decbbbc31fda6313ff639142d3
Martin Pilaszek <martinpilaszek@gmail.com>
2021-11-16 18:59:10 -0300

    Fixed menu font and close on click

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,19 +1,26 @@
 <template>
-  <form @submit.prevent="validation">
+  <form @submit.prevent="submit">
     <h2>Contact</h2>
     <label for="name">Name</label>
-    <input v-model="name" type="text" id="name" name="name">
+    <input v-model="name" type="text" id="name" name="name" />
     <label for="email">Email</label>
-    <input v-model="email" type="text" id="email" name="email">
+    <input v-model="email" type="text" id="email" name="email" />
     <label for="message">Message</label>
-    <textarea v-model="message" name="message" id="message" cols="30" rows="10"></textarea>
+    <textarea
+      v-model="msg"
+      name="message"
+      id="message"
+      cols="30"
+      rows="10"
+    ></textarea>
     <Button type="submit" mt="1rem" color="secondary" :full="true">Send</Button>
-    <span :class="alertStyles">{{alertMsg}}</span>
+    <span :class="alertStyles">{{ alertMsg }}</span>
   </form>
 </template>
 
 <script>
 import Button from '@/components/Button.vue'
+import { ref, computed } from 'vue'
 
 const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g
 const endpoint = 'https://formspree.io/f/xdoyegpk'
@@ -21,107 +28,110 @@ const endpoint = 'https://formspree.io/f/xdoyegpk'
 export default {
   name: 'ContactForm',
   components: { Button },
-  data () {
-    return {
-      name: '',
-      email: '',
-      message: '',
-      alert: false,
-      alertType: '',
-      alertMsg: '',
-      loading: false
-    }
-  },
-  computed: {
-    alertStyles () {
+  setup () {
+    const name = ref('')
+    const email = ref('')
+    const msg = ref('')
+    const loading = ref(false)
+    const alert = ref(false)
+    const alertMsg = ref({})
+    const alertStyles = computed(() => {
       return {
-        visible: this.alert === true,
-        error: this.alertType === 'error',
-        success: this.alertType === 'success'
+        visible: alert.value,
+        error: alertMsg.value.type === 'error',
+        success: alertMsg.value.type === 'success'
       }
-    }
-  },
-  methods: {
-    clearAlert () {
+    })
+    const clearAlert = () => {
       setTimeout(() => {
-        this.alert = false
-        this.alertType = ''
-        this.alertMsg = ''
+        alert.value = false
+        alertMsg.value = {}
       }, 5000)
-    },
-    async submit () {
-      this.loading = true
+    }
+    const submit = async () => {
+      loading.value = true
       await fetch(endpoint, {
         method: 'POST',
-        body: JSON.stringify({ name: this.name, email: this.email, message: this.message }),
+        body: JSON.stringify({
+          name: this.name,
+          email: this.email,
+          message: this.message
+        }),
         headers: {
           Accept: 'application/json'
         }
       })
         .then(() => {
-          this.alert = true
-          this.alertType = 'success'
-          this.alertMsg = 'Sent! Thank you'
+          alert.value = true
+          alertMsg.value = { type: 'success', msg: 'Sent! Thank you' }
         })
         .catch(() => {
-          this.alert = true
-          this.alertType = 'error'
-          this.alertMsg = 'An error has ocurred, please try again'
+          alert.value = true
+          alertMsg.value = { type: 'error', msg: 'An error has ocurred, please try again' }
         })
         .finally(() => {
-          this.loading = false
-          this.clearAlert()
+          loading.value = false
+          clearAlert()
         })
-    },
-    validation () {
-      if (!this.email | !this.name | !this.message) {
-        this.alert = true
-        this.alertType = 'error'
-        this.alertMsg = 'Please fill all the fields'
-        return this.clearAlert()
+    }
+    const validation = () => {
+      if (!email.value | !name.value | !msg.value) {
+        alert.value = true
+        alertMsg.value = { type: 'error', msg: 'Please fill all the fields' }
+        return clearAlert()
       } else if (!emailRegex.test(this.email)) {
-        this.alert = true
-        this.alertType = 'error'
-        this.alertMsg = 'Please put a valid email'
-        return this.clearAlert()
+        alert.value = true
+        alertMsg.value = { type: 'error', msg: 'Please put a valid email' }
+        return clearAlert()
       } else {
-        this.submit()
+        submit()
       }
+    }
+
+    return {
+      name,
+      email,
+      msg,
+      validation,
+      loading,
+      alertStyles
     }
   }
 }
 </script>
 
 <style scoped>
-h2{
+h2 {
   color: #c8ff30;
-  margin-bottom: .5rem;
+  margin-bottom: 0.5rem;
 }
-label{
+label {
   display: block;
-  margin-bottom: .3rem;
+  margin-bottom: 0.3rem;
   color: #c8ff30;
 }
-input, textarea{
+input,
+textarea {
   display: block;
   width: 100%;
   height: 2.5rem;
   border-radius: 2rem;
   border: none;
   padding: 0 1rem;
-  margin-bottom: .6rem;
+  margin-bottom: 0.6rem;
 }
-input:focus, textarea:focus{
+input:focus,
+textarea:focus {
   border: 2px solid #c8ff30;
   outline: none;
 }
-textarea{
+textarea {
   border-radius: 1rem;
   height: 10rem;
   resize: none;
-  padding: .8rem;
+  padding: 0.8rem;
 }
-form{
+form {
   background-color: #052b36;
   padding: 1rem;
   border-radius: 1rem;
@@ -129,11 +139,11 @@ form{
 }
 
 /* alert */
-span{
+span {
   font-weight: 600;
   position: absolute;
   text-align: center;
-  padding: .75rem;
+  padding: 0.75rem;
   left: 0;
   right: 0;
   margin-top: 1.5rem;
@@ -143,7 +153,7 @@ span{
   transform: translateY(20px);
   opacity: 0;
 }
-.visible{
+.visible {
   transform: translateY(0);
   opacity: 1;
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="menu" :class="{menuOn: menu, menuOff: !menu}">
+  <div class="menu" :class="{ menuOn: menu, menuOff: !menu }">
     <button @click="menuOff" class="menu_off">
-      <img src="https://icongr.am/feather/x.svg?size=128&color=052b36" alt="Close menu">
+      <img
+        src="https://icongr.am/feather/x.svg?size=128&color=052b36"
+        alt="Close menu"
+      />
     </button>
-    <router-link to="/">Home</router-link>
-    <router-link to="/projects">Projects</router-link>
-    <router-link to="/contact">Contact</router-link>
+    <router-link @click="menu = !menu" to="/">Home</router-link>
+    <router-link @click="menu = !menu" to="/projects">Projects</router-link>
+    <router-link @click="menu = !menu" to="/contact">Contact</router-link>
   </div>
   <header>
     <div class="navbar_container">
       <router-link to="/">
-        <img class="logo" src="/images/logo-azul.svg" alt="PZK Logo">
+        <img class="logo" src="/images/logo-azul.svg" alt="PZK Logo" />
       </router-link>
       <nav>
         <router-link to="/">Home</router-link>
@@ -18,7 +21,10 @@
         <router-link to="/contact">Contact</router-link>
       </nav>
       <button @click="menuOn" class="menu_on">
-        <img src="https://icongr.am/feather/menu.svg?size=128&color=052b36" alt="Menu">
+        <img
+          src="https://icongr.am/feather/menu.svg?size=128&color=052b36"
+          alt="Menu"
+        />
       </button>
     </div>
   </header>
@@ -47,41 +53,41 @@ export default {
 </script>
 
 <style scoped>
-header{
+header {
   height: 50px;
   width: 95%;
   max-width: 960px;
   margin: 0 auto;
   font-weight: 600;
 }
-header>div{
+header > div {
   height: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-button>img{
+button > img {
   height: 22px;
 }
-.menu_on{
+.menu_on {
   background-color: transparent;
   border: none;
   cursor: pointer;
   padding: 0;
 }
-.logo{
+.logo {
   width: 75px;
 }
-nav{
+nav {
   display: none;
 }
-nav>:not(:nth-child(1)){
+nav > :not(:nth-child(1)) {
   margin-left: 2rem;
 }
-nav>a{
+nav > a {
   position: relative;
 }
-nav>::after{
+nav > ::after {
   content: "";
   height: 0.3rem;
   background-color: #c8ff30;
@@ -94,25 +100,27 @@ nav>::after{
   transform-origin: left;
   /* color: #c8ff30; */
 }
-nav>:hover::after{
+nav > :hover::after {
   transform: scaleX(1);
 }
 
-@media screen and (min-width: 720px){
-  .menu_on{
+@media screen and (min-width: 720px) {
+  .menu_on {
     display: none;
   }
-  nav{
+  nav {
     display: block;
   }
 }
 
-.menu{
+.menu {
   background-color: white;
   height: 100vh;
   width: 100vw;
+  font-weight: 600;
+  font-size: 22px;
 }
-.menuOn{
+.menuOn {
   background-color: white;
   height: 100vh;
   width: 100vw;
@@ -124,10 +132,11 @@ nav>:hover::after{
   z-index: 99;
   transition: transform 1s ease;
 }
-.menu>:not(:first-child), .menu>:not(:last-child){
+.menu > :not(:first-child),
+.menu > :not(:last-child) {
   margin-bottom: 2rem;
 }
-.menuOff{
+.menuOff {
   position: fixed;
   height: 100vh;
   width: 100vw;
@@ -139,10 +148,11 @@ nav>:hover::after{
   transform: translateX(1000%);
   transition: transform 2s ease;
 }
-.menu>:not(:first-child){
+.menu > :not(:first-child) {
   display: inline;
 }
-.menu_off{
+.menu_off {
+  cursor: pointer;
   background: none;
   border: none;
   padding: 0;
@@ -150,5 +160,4 @@ nav>:hover::after{
   top: 1rem;
   right: 2rem;
 }
-
 </style>
